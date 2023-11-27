de0ffc69b9aac1702bf55af73006a63d5cc9ee34
sifi-border <shusaku.fujihara@access-company.com>
2023-11-28 00:14:07 +0900

    Add main.rs file with basic routing and handlers

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::get, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = create_app();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 0815,
+        username: payload.username,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{body::Body, http::Request};
+    use hyper::{header, Method};
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder()
+            .uri("/")
+            .body(hyper::Body::empty())
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .uri("/users")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(r#"{"username": "yui ogura"}"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        let user: User = serde_json::from_str(&body).expect("failed to convert User instance");
+        assert_eq!(
+            user,
+            User {
+                id: 815,
+                username: "yui ogura".to_string(),
+            }
+        );
+    }
+}
