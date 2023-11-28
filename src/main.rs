47c77eac935e0a09eb729d39726fc33dbe31d497
sifi-border <shusaku.fujihara@access-company.com>
2023-11-29 00:23:23 +0900

    Remove create_user endpoint

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,6 @@ async fn main() {
 fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user))
         .route("/todos", post(create_todo::<T>))
         .layer(Extension(Arc::new(repository)))
 }
@@ -123,25 +122,6 @@ pub async fn create_todo<T: TodoRepository>(
 
 async fn root() -> &'static str {
     "Hello, World!"
-}
-
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    let user = User {
-        id: 0815,
-        username: payload.username,
-    };
-    (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
 }
 
 #[cfg(test)]
@@ -162,27 +142,5 @@ mod tests {
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, World!");
-    }
-
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let repository = TodoRepositoryForMemory::new();
-        let req = Request::builder()
-            .uri("/users")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(r#"{"username": "yui ogura"}"#))
-            .unwrap();
-        let res = create_app(repository).oneshot(req).await.unwrap();
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-        let user: User = serde_json::from_str(&body).expect("failed to convert User instance");
-        assert_eq!(
-            user,
-            User {
-                id: 815,
-                username: "yui ogura".to_string(),
-            }
-        );
     }
 }
