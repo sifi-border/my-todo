39ab2705ee37e27f151642bbcb7c54fa7d6060e7
sifi-border <shusaku.fujihara@access-company.com>
2023-11-29 00:40:25 +0900

    Remove unused import

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,7 @@
 use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
 use std::sync::Arc;
 
-use crate::repositories::{CreateTodo, Todo, TodoRepository};
+use crate::repositories::{CreateTodo, TodoRepository};
 pub async fn create_todo<T: TodoRepository>(
     Json(payload): Json<CreateTodo>,
     Extension(repository): Extension<Arc<T>>,
