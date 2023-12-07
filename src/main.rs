38bb982f284475c02fe7a8b13526cb6ac5ecd042
sifi-border <shusaku.fujihara@access-company.com>
2023-12-08 02:11:29 +0900

    Add tower-http crate and enable CORS support

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use axum::extract::Extension;
 use axum::{routing::get, routing::post, Router};
 use dotenv::dotenv;
 use handlers::{all_todo, create_todo, delete_todo, find_todo, update_todo};
+use hyper::header::CONTENT_TYPE;
 use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use tower_http::cors::{Any, CorsLayer, Origin};
 
 #[tokio::main]
 async fn main() {
@@ -47,6 +49,12 @@ fn create_app<T: TodoRepository>(repository: T) -> Router {
                 .patch(update_todo::<T>),
         )
         .layer(Extension(Arc::new(repository)))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Origin::exact("http://localhost:3001".parse().unwrap()))
+                .allow_methods(Any)
+                .allow_headers(vec![CONTENT_TYPE]),
+        )
 }
 
 async fn root() -> &'static str {
