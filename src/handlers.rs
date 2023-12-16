1faaaf0e188407b0de8f36ffb2472b63698da746
sifi-border <shusaku.fujihara@access-company.com>
2023-12-17 00:49:18 +0900

    Add validation for JSON payloads in request handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,1 +1,36 @@
+use axum::{
+    async_trait,
+    extract::{FromRequest, RequestParts},
+    BoxError, Json,
+};
+use hyper::StatusCode;
+use serde::de::DeserializeOwned;
+use validator::Validate;
+
 pub mod todo;
+
+#[derive(Debug)]
+pub struct ValidatedJson<T>(T);
+
+#[async_trait]
+impl<T, B> FromRequest<B> for ValidatedJson<T>
+where
+    T: DeserializeOwned + Validate,
+    B: http_body::Body + Send,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let Json(value) = Json::<T>::from_request(req).await.map_err(|rejection| {
+            let message = format!("Json parse error: [{}]", rejection);
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        value.validate().map_err(|rejection| {
+            let message = format!("Validation error: [{}]", rejection).replace('\n', " ");
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        Ok(ValidatedJson(value))
+    }
+}
