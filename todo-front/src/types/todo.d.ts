8fdf1866b5671d8ecbee65d514385e6970e3cb86
sifi-border <shusaku.fujihara@access-company.com>
2024-01-14 06:56:00 +0900

    Add SideNav component and label API functions

--- todo-front/src/types/todo.d.ts
+++ todo-front/src/types/todo.d.ts
@@ -2,8 +2,22 @@ export type Todo = {
   id: number;
   text: string;
   completed: boolean;
+  labels: Label[];
 };
 
 export type NewTodoPayload = {
   text: string;
+};
+
+export type UpdateTodoPayload = Partial<Omit<Todo, "id">> & {
+  id: number;
+};
+
+export type Label = {
+  id: number;
+  name: string;
+};
+
+export type NewLabelPayload = {
+  name: string;
 };
